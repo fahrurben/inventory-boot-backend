5f271ead69979b585b7b052b16a4c91268c60cdf
fahrur <fahrurben@gmail.com>
2018-05-04 14:55:04 +0700

    Spring hello

---
+++ src/main/java/com/kyrosoft/inventory/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.kyrosoft.inventory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello";
+    }
+}
