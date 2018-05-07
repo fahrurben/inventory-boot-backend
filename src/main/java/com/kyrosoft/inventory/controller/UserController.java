ae54c406a744743af8c64a4e66307ffa200dce47
fahrur <fahrurben@gmail.com>
2018-05-07 10:03:18 +0700

    Spring security initial

---
+++ src/main/java/com/kyrosoft/inventory/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.kyrosoft.inventory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/api/user/me")
+    public Principal user(Principal principal) {
+        System.out.println(principal);
+        return principal;
+    }
+
+    @RequestMapping("/api/me")
+    public Principal me(Principal principal) {
+        System.out.println(principal);
+        return principal;
+    }
+}
